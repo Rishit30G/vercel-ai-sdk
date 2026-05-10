ede9cd0311b5133b2804806555cb76497d6dd2bc
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2026-05-10 21:58:51 +0530

    Refactor code formatting and improve readability in CompletionPage component

--- app/ui/stream/page.tsx
+++ app/ui/stream/page.tsx
@@ -1,16 +1,24 @@
 'use client';
 
-import {useCompletion} from "@ai-sdk/react"
+import { useCompletion } from '@ai-sdk/react';
 
 export default function CompletionPage() {
-    const {input, handleInputChange, handleSubmit, completion, isLoading, error, stop} = useCompletion({
-        api: "/api/stream"
-    })
+  const {
+    input,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+    error,
+    stop,
+  } = useCompletion({
+    api: '/api/stream',
+  });
   return (
     <div className="min-h-screen flex flex-col">
-        {error && <div className="text-red-500 mb-4"> {error.message} </div>}
-        {isLoading && !completion && <div>Loading...</div>}
-        {completion && <div>{completion}</div>}
+      {error && <div className="text-red-500 mb-4"> {error.message} </div>}
+      {isLoading && !completion && <div>Loading...</div>}
+      {completion && <div>{completion}</div>}
       <form onSubmit={handleSubmit} className="fixed bottom-10 left-0 right-0">
         <div className="flex justify-center items-baseline gap-2">
           <input
@@ -20,15 +28,19 @@ export default function CompletionPage() {
             onChange={handleInputChange}
           />
 
-          {isLoading ? <button onClick={stop} className="bg-red-500 py-2 px-4 rounded-xl">Stop</button> : 
-          <button
-            type="submit"
-            className="bg-blue-500 py-2 px-4 rounded-xl"
-            disabled={isLoading}
-          >
-            Send
-          </button>
-}
+          {isLoading ? (
+            <button onClick={stop} className="bg-red-500 py-2 px-4 rounded-xl">
+              Stop
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="bg-blue-500 py-2 px-4 rounded-xl"
+              disabled={isLoading}
+            >
+              Send
+            </button>
+          )}
         </div>
       </form>
     </div>
